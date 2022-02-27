35798d72ff66f7d4d93503c88cbf438bc2ad12d1
matty <mtang317@gmail.com>
2022-02-27 17:47:07 -0600

    Added direction and speed tracking

--- Lab2/frontend_tutorial/wifi_server.py
+++ Lab2/frontend_tutorial/wifi_server.py
@@ -14,6 +14,8 @@ signal.signal(signal.SIGINT, signal_handler)
 
 STANDARD_SUFFIX = "\r\n"
 SPEED = 15
+direction = ""
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -23,24 +25,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         client, clientInfo = s.accept()
         while 1:
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
+
             if data != b"":
+
                 print(data)
                 if data == b"87":
                     # w, up
                     fc.forward(SPEED)
+                    direction = "Forward"
                 elif data == b"83":
                     # s, down
                     fc.backward(SPEED)
+                    direction = "Backward"
                 elif data == b"65":
                     # a, left
                     fc.turn_left(SPEED)
+                    direction = "LEFT"
                 elif data == b"68":
                     # d, right
                     fc.turn_right(SPEED)
+                    direction = "Right"
                 elif data == b"STOP":
                     fc.stop()
-                else:
+                    direction = ""
 
+                else:
                     client.sendall(data) # Echo back to client
     except: 
         print("Closing socket")
