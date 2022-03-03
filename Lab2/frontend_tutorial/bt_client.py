5360b98ee25f1c6d74afc02da0ab60ea2da4367b
matty <mtang317@gmail.com>
2022-03-03 10:36:38 -0600

    Finish BT

--- Lab2/frontend_tutorial/bt_client.py
+++ Lab2/frontend_tutorial/bt_client.py
@@ -1,17 +1,17 @@
 import bluetooth
 
-host = "DC:A6:32:80:7D:87" # The address of Raspberry PI Bluetooth adapter on the server.
+host = "B8:27:EB:34:24:F0" # The address of Raspberry PI Bluetooth adapter on the server.
 port = 1
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
 while 1:
-    text = input("Enter your message: ") # Note change to the old (Python 2) raw_input
+    text = input("> ") # Note change to the old (Python 2) raw_input
     if text == "quit":
         break
     sock.send(text)
 
     data = sock.recv(1024)
-    print("from server: ", data)
+    print("Car: ", data)
 
 sock.close()
 
--- Lab2/frontend_tutorial/bt_server.py
+++ Lab2/frontend_tutorial/bt_server.py
@@ -1,4 +1,15 @@
 import bluetooth
+import picar_4wd as fc
+import signal
+import sys
+#Interrupt handler
+def signal_handler(signal, frame):
+    fc.stop()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+
+power_val = 50
 
 hostMACAddress = "B8:27:EB:34:24:F0" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
 port = 0
@@ -11,13 +22,29 @@ print("listening on port ", port)
 try:
     client, clientInfo = s.accept()
     while 1:   
-        print("server recv from: ", clientInfo)
         data = client.recv(size)
         if data:
-            print(data)
+            key = data.decode('utf-8')
+            print(key)
+            if key=='w':
+                fc.forward(power_val)
+            elif key=='a':
+                fc.turn_left(power_val)
+            elif key=='s':
+                fc.backward(power_val)
+            elif key=='d':
+                fc.turn_right(power_val)
+            else:
+                fc.stop()
+            if key=='q':
+                print("quit")  
+                break  
+
             client.send(data) # Echo back to client
-except: 
+except Exception as E:
+    print(E) 
     print("Closing socket")
+    fc.stop()
     client.close()
     s.close()
 
